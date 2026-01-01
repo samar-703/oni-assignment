ff71baf2c72a02727d85bf0663ff6d7075826ecc
samaR <hello.samar7@gmail.com>
2026-01-01 11:23:12 +0000

    added AppNavigator for managing navigation between Home and Counter screens

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+import HomeScreen from "../screens/HomeScreen";
+import CounterScreen from "../screens/CounterScreen";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Counter" component={CounterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
